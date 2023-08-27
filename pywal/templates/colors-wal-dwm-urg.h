3ebf40734523a4e25d18a7c2bd6be2ac3a08b4ba
Mohamed Tarek <mohamednafagr@gmail.com>
2023-08-27 09:55:44 +0300

    handle dwm urgent patch in a new file

--- pywal/templates/colors-wal-dwm-urg.h
+++ pywal/templates/colors-wal-dwm-urg.h
@@ -6,8 +6,13 @@ static const char sel_fg[] = "{color15}";
 static const char sel_bg[] = "{color2}";
 static const char sel_border[] = "{color15}";
 
+static const char urg_fg[] = "{color15}";
+static const char urg_bg[] = "{color1}";
+static const char urg_border[] = "{color1}";
+
 static const char *colors[][3]      = {{
     /*               fg           bg         border                         */
     [SchemeNorm] = {{ norm_fg,     norm_bg,   norm_border }}, // unfocused wins
     [SchemeSel]  = {{ sel_fg,      sel_bg,    sel_border }},  // the focused win
+    [SchemeUrg] =  {{ urg_fg,      urg_bg,    urg_border }},
 }};
