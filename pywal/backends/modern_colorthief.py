d7aa13811b5d377843365a0b2142026e15e9ab78
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-15 15:03:30 +0600

    Create modern_colorthief.py

---
+++ pywal/backends/modern_colorthief.py
@@ -0,0 +1,38 @@
+"""
+Generate a colorscheme using fast_colorthief.
+"""
+
+import logging
+import sys
+
+try:
+    import modern_colorthief
+
+except ImportError:
+    logging.error("modern_colorthief wasn't found on your system.")
+    logging.error("Try another backend. (wal --backend)")
+    sys.exit(1)
+
+from .. import util
+from .. import colors
+
+
+def gen_colors(img):
+    """Ask backend to generate 16 colors."""
+    raw_colors = modern_colorthief.get_palette(img, 16)
+
+    return [util.rgb_to_hex(color) for color in raw_colors]
+
+
+def adjust(cols, light, cols16):
+    """Create palette."""
+    cols.sort(key=util.rgb_to_yiq)
+    raw_colors = [*cols, *cols]
+
+    return colors.generic_adjust(raw_colors, light, cols16)
+
+
+def get(img, light=False, cols16=False):
+    """Get colorscheme."""
+    cols = gen_colors(img)
+    return adjust(cols, light, cols16)
