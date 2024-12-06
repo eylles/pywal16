d1ded83dfb0e819fecf57486d6328b42ae6bc597
eylles <ed.ylles1997@gmail.com>
2024-12-06 08:25:21 -0600

    Remove reloading of gtk
    Python2 is obsolte, not to mention this method only provided support for
    reloading gtk2.

--- pywal/reload.py
+++ pywal/reload.py
@@ -30,19 +30,6 @@ def xrdb(xrdb_files=None):
             subprocess.run(["xrdb", "-merge", "-quiet", file], check=False)
 
 
-def gtk():
-    """Reload GTK theme on the fly."""
-    # Here we call a Python 2 script to reload the GTK themes.
-    # This is done because the Python 3 GTK/Gdk libraries don't
-    # provide a way of doing this.
-    if shutil.which("python2"):
-        gtk_reload = os.path.join(MODULE_DIR, "scripts", "gtk_reload.py")
-        util.disown(["python2", gtk_reload])
-
-    else:
-        logging.warning("GTK2 reload support requires Python 2.")
-
-
 def i3():
     """Reload i3 colors."""
     if shutil.which("i3-msg") and util.get_pid("i3"):
