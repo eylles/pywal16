30a9364f46059f5fa080d05e7f0d32e0d49e8a18
mistekko <steamlie@protonmail.com>
2025-01-23 14:25:20 -0600

    Change dwm selection bg colour to match dmenu's

--- pywal/templates/colors-wal-dwm.h
+++ pywal/templates/colors-wal-dwm.h
@@ -3,7 +3,7 @@ static const char norm_bg[] = "{color0}";
 static const char norm_border[] = "{color8}";
 
 static const char sel_fg[] = "{color15}";
-static const char sel_bg[] = "{color2}";
+static const char sel_bg[] = "{color1}";
 static const char sel_border[] = "{color15}";
 
 static const char *colors[][3]      = {{
