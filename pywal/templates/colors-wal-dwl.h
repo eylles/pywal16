fb358b455f25ec429d10ec361abbf151ae1e946b
λmolinae <65742076+amolinae06@users.noreply.github.com>
2025-02-21 13:19:00 -0600

    Add files via upload

---
+++ pywal/templates/colors-wal-dwl.h
@@ -0,0 +1,14 @@
+#define WALLPAPER "{wallpaper}"
+/* Taken from https://github.com/djpohly/dwl/issues/466 */
+#define COLOR(hex)    {{ ((hex >> 24) & 0xFF) / 255.0f, \
+                        ((hex >> 16) & 0xFF) / 255.0f, \
+                        ((hex >> 8) & 0xFF) / 255.0f, \
+                        (hex & 0xFF) / 255.0f }}
+
+static const float rootcolor[]             = COLOR(0x{color0.strip}ff);
+static uint32_t colors[][3]                = {{
+	/*               fg          bg          border    */
+	[SchemeNorm] = {{ 0x{color15.strip}ff, 0x{color0.strip}ff, 0x{color8.strip}ff }},
+	[SchemeSel]  = {{ 0x{color15.strip}ff, 0x{color2.strip}ff, 0x{color1.strip}ff }},
+	[SchemeUrg]  = {{ 0x{color15.strip}ff, 0x{color1.strip}ff, 0x{color2.strip}ff }},
+}};
