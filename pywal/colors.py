4a85d4c98e9e52d676c208ba8d5fc5f9c852d43e
eylles <ed.ylles1997@gmail.com>
2025-05-24 12:37:29 -0600

    Allow saturate_colors to effect colors 0 and 8

--- pywal/colors.py
+++ pywal/colors.py
@@ -185,7 +185,7 @@ def saturate_colors(colors, amount):
     """Saturate all colors."""
     if amount and float(amount) <= 1.0:
         for i, _ in enumerate(colors):
-            if i not in [0, 7, 8, 15]:
+            if i not in [7, 15]:
                 colors[i] = util.saturate_color(colors[i], float(amount))
 
     return colors
