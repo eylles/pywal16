b226d88a237acfb511d7c177d026363819696a8b
eylles <ed.ylles1997@gmail.com>
2025-11-28 10:25:10 -0600

    Add option to disable donations message

--- pywal/donation.py
+++ pywal/donation.py
@@ -1,13 +1,18 @@
 import datetime
 import logging
+import os
 
 
 def donation_message():
     url = "https://github.com/eylles/pywal16"
     donate_msg = "if you enjoy pywal16 consider donating at: {}".format(url)
-    if datetime.datetime.now().month == 12:
-        logging.info(donate_msg)
-    else:
-        msec_mod = datetime.datetime.now().microsecond % 20
-        if msec_mod >= 6:
+    no_donations = int(os.getenv(
+        "JOBLESS_FAT_BASTARD_STOP_BEGGING_FOR_MONEY", 0
+        ))
+    if not no_donations:
+        if datetime.datetime.now().month == 12:
             logging.info(donate_msg)
+        else:
+            msec_mod = datetime.datetime.now().microsecond % 20
+            if msec_mod >= 6:
+                logging.info(donate_msg)
