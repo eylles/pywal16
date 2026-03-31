658886ddffc0dff30f8599a7cba15f7cfe854e8e
Finn Harbeke <code@finnharbeke.ch>
2026-03-31 15:26:39 +0200

    adapt dwl template to up-to-date config.def.h
    https://github.com/djpohly/dwl has been archived and the currently maintained repository is on codeberg, https://codeberg.org/dwl/dwl
    
    so i think it would be proper to adjust this template to the active dwl repository

--- pywal/templates/colors-wal-dwl.h
+++ pywal/templates/colors-wal-dwl.h
@@ -5,9 +5,6 @@
                         (hex & 0xFF) / 255.0f }}
 
 static const float rootcolor[]             = COLOR(0x{color0.strip}ff);
-static uint32_t colors[][3]                = {{
-	/*               fg          bg          border    */
-	[SchemeNorm] = {{ 0x{color15.strip}ff, 0x{color0.strip}ff, 0x{color8.strip}ff }},
-	[SchemeSel]  = {{ 0x{color15.strip}ff, 0x{color2.strip}ff, 0x{color1.strip}ff }},
-	[SchemeUrg]  = {{ 0x{color15.strip}ff, 0x{color1.strip}ff, 0x{color2.strip}ff }},
-}};
+static const float bordercolor[]           = COLOR(0x{color2.strip}ff);
+static const float focuscolor[]            = COLOR(0x{color1.strip}ff);
+static const float urgentcolor[]           = COLOR(0x{color3.strip}ff);
